b9e581c0b29a415af970ead02468debd05b12e66
AdamasTheCreator <106996356+AdamasTheCreator@users.noreply.github.com>
2023-01-15 14:41:54 -0500

    Update GeneralPlayerControl.cs

--- GeneralPlayerControl.cs
+++ GeneralPlayerControl.cs
@@ -40,21 +40,16 @@ public class CharacterController2D : MonoBehaviour
 
     public void Swim(float swim)
 	{
-			// Move the character by finding the target velocity
 			Vector3 targetVelocity = new Vector2(swim * 10f, m_Rigidbody2D.velocity.y);
-			// And then smoothing it out and applying it to the character
+
 			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref velocity, m_MovementSmoothing);
 
-			// If the input is moving the player right and the player is facing left...
 			if (swim > 0 && !m_FacingRight)
 			{
-				// ... flip the player.
 				Flip();
 			}
-			// Otherwise if the input is moving the player left and the player is facing right...
 			else if (swim < 0 && m_FacingRight)
 			{
-				// ... flip the player.
 				Flip();
 			}
 	}
@@ -62,40 +57,35 @@ public class CharacterController2D : MonoBehaviour
 
 	public void Sprint(float sprint)
     {
-			// Move the character by finding the target velocity
+
 			Vector3 targetVelocity = new Vector2(sprint * 10f, m_Rigidbody2D.velocity.y);
-			// And then smoothing it out and applying it to the character
+
 			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref velocity, m_MovementSmoothing);
 
-			// If the input is moving the player right and the player is facing left...
+
 			if (sprint > 0 && !m_FacingRight)
 			{
-				// ... flip the player.
 				Flip();
 			}
-			// Otherwise if the input is moving the player left and the player is facing right...
 			else if (sprint < 0 && m_FacingRight)
 			{
-				// ... flip the player.
 				Flip();
 			}
 	}
 
 	public void Move(float move, bool crouch, bool jump)
 	{
-			// Move the character by finding the target velocity
+	
 			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
-			// And then smoothing it out and applying it to the character
+
 			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref velocity, m_MovementSmoothing);
 
 			if (move > 0 && !m_FacingRight)
 			{
-				// ... flip the player.
 				Flip();
 			}
 			else if (move < 0 && m_FacingRight)
 			{
-				// ... flip the player.
 				Flip();
 			}
 	}
@@ -103,10 +93,9 @@ public class CharacterController2D : MonoBehaviour
 
 	public void Flip()
 	{
-		// Switch the way the player is labelled as facing.
+
 		m_FacingRight = !m_FacingRight;
 
-		// Multiply the player's x local scale by -1.
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
